27380a875e511d18117eec66777d9adf596c1b25
celia22 <celialopez22@gmail.com>
2021-05-03 16:25:41 +0200

    finish IT 1

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,6 +8,8 @@ const saltRounds = 10;
 
 const User = require('../models/User.model');
 
+// SIGNUP //
+
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
 router.post('/signup', (req, res, next) => {
@@ -34,5 +36,33 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+// LOGIN //
+
+// router.get('/login', (req, res) => res.render('auth/login'));
+
+// router.post('/login', (req, res, next) => {
+//   const { email, password } = req.body;
+
+//   if (email === '' || password === '') {
+//     res.render('auth/login', {
+//       errorMessage: 'Please enter both, email and password to login.',
+//     });
+//     return;
+//   }
+
+//   User.findOne({ email })
+//     .then(user => {
+//       if (!user) {
+//         res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+//         return;
+//       } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+//         res.render('users/user-profile', { user });
+//       } else {
+//         res.render('auth/login', { errorMessage: 'Incorrect password.' });
+//       }
+//     })
+//     .catch(error => next(error));
+// });
 
 module.exports = router;
